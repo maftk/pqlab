e70521659d2b33420d74b2752c8f420c741e2561
maftk <m.fuji@team-shiny.org>
2024-09-14 10:33:02 +0900

    modify

--- README.md
+++ README.md
@@ -20,3 +20,23 @@ Pythonコンテナの操作 Python環境を操作するには、次のコマン�
 docker exec -it py bash
 
 ```
+
+読み込んでいて気付いたこと
+`covit19_sum.sql`,`pref_code.sql`,`make_covit19_2.sql`から`init.sql`
+を作った。
+```sql
+--covit19_sum.sql
+CREATE TABLE covid19_sum (--covit19_sum > covid19_sum
+
+--pref_code.sql
+--pref_name列に入る漢字の文字化けを修正
+--修正前
+INSERT INTO pref_code VALUES('01','kC¹','Hokkaido');
+--修正後
+INSERT INTO pref_code VALUES('01','北海道','Hokkaido');
+
+
+--make_covit19_2.sql
+--covit19.sum.sqlにより作られたテーブル名が間違ってた
+covid19_sum AS c --最初に見つけたエラー部分
+```
--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,5 @@
-CREATE TABLE covid19_sum (
+--covit19_sum.sql
+CREATE TABLE covid19_sum (--covit19_sum > covid19_sum
   reporting_date DATE
   , nationwide INTEGER
   , hokkaido INTEGER
@@ -50,7 +51,7 @@ CREATE TABLE covid19_sum (
   , okinawa INTEGER
 );
 
-
+--pref_code.sql
 CREATE TABLE pref_code
 (
   pref_code	CHAR(2),
@@ -58,55 +59,56 @@ CREATE TABLE pref_code
   eng_pref_name	VARCHAR(16)
 )
 ;
+--pref_name列に入る漢字の文字化けを修正
+INSERT INTO pref_code VALUES('01','北海道','Hokkaido');
+INSERT INTO pref_code VALUES('02','青森県','Aomori');
+INSERT INTO pref_code VALUES('03','岩手県','Iwate');
+INSERT INTO pref_code VALUES('04','宮城県','Miyagi');
+INSERT INTO pref_code VALUES('05','秋田県','Akita');
+INSERT INTO pref_code VALUES('06','山形県','Yamagata');
+INSERT INTO pref_code VALUES('07','福島県','Fukushima');
+INSERT INTO pref_code VALUES('08','茨城県','Ibaraki');
+INSERT INTO pref_code VALUES('09','栃木県','Tochigi');
+INSERT INTO pref_code VALUES('10','群馬県','Gunma');
+INSERT INTO pref_code VALUES('11','埼玉県','Saitama');
+INSERT INTO pref_code VALUES('12','千葉県','Chiba');
+INSERT INTO pref_code VALUES('13','東京都','Tokyo');
+INSERT INTO pref_code VALUES('14','神奈川県','Kanagawa');
+INSERT INTO pref_code VALUES('15','新潟県','Niigata');
+INSERT INTO pref_code VALUES('16','富山県','Toyama');
+INSERT INTO pref_code VALUES('17','石川県','Ishikawa');
+INSERT INTO pref_code VALUES('18','福井県','Fukui');
+INSERT INTO pref_code VALUES('19','山梨県','Yamanashi');
+INSERT INTO pref_code VALUES('20','長野県','Nagano');
+INSERT INTO pref_code VALUES('21','岐阜県','Gifu');
+INSERT INTO pref_code VALUES('22','静岡県','Shizuoka');
+INSERT INTO pref_code VALUES('23','愛知県','Aichi');
+INSERT INTO pref_code VALUES('24','三重県','Mie');
+INSERT INTO pref_code VALUES('25','滋賀県','Shiga');
+INSERT INTO pref_code VALUES('26','京都府','Kyoto');
+INSERT INTO pref_code VALUES('27','大阪府','Osaka');
+INSERT INTO pref_code VALUES('28','兵庫県','Hyogo');
+INSERT INTO pref_code VALUES('29','奈良県','Nara');
+INSERT INTO pref_code VALUES('30','和歌山県','Wakayama');
+INSERT INTO pref_code VALUES('31','鳥取県','Tottori');
+INSERT INTO pref_code VALUES('32','島根県','Shimane');
+INSERT INTO pref_code VALUES('33','岡山県','Okayama');
+INSERT INTO pref_code VALUES('34','広島県','Hiroshima');
+INSERT INTO pref_code VALUES('35','山口県','Yamaguchi');
+INSERT INTO pref_code VALUES('36','徳島県','Tokushima');
+INSERT INTO pref_code VALUES('37','香川県','Kagawa');
+INSERT INTO pref_code VALUES('38','愛媛県','Ehime');
+INSERT INTO pref_code VALUES('39','高知県','Kochi');
+INSERT INTO pref_code VALUES('40','福岡県','Fukuoka');
+INSERT INTO pref_code VALUES('41','佐賀県','Saga');
+INSERT INTO pref_code VALUES('42','長崎県','Nagasaki');
+INSERT INTO pref_code VALUES('43','熊本県','Kumamoto');
+INSERT INTO pref_code VALUES('44','大分県','Oita');
+INSERT INTO pref_code VALUES('45','宮崎県','Miyazaki');
+INSERT INTO pref_code VALUES('46','鹿児島県','Kagoshima');
+INSERT INTO pref_code VALUES('47','沖縄県','Okinawa');
 
-INSERT INTO pref_code VALUES('01','kC¹','Hokkaido');
-INSERT INTO pref_code VALUES('02','ÂX§','Aomori');
-INSERT INTO pref_code VALUES('03','âè§','Iwate');
-INSERT INTO pref_code VALUES('04','{é§','Miyagi');
-INSERT INTO pref_code VALUES('05','Hc§','Akita');
-INSERT INTO pref_code VALUES('06','R`§','Yamagata');
-INSERT INTO pref_code VALUES('07','§','Fukushima');
-INSERT INTO pref_code VALUES('08','ïé§','Ibaraki');
-INSERT INTO pref_code VALUES('09','ÈØ§','Tochigi');
-INSERT INTO pref_code VALUES('10','Qn§','Gunma');
-INSERT INTO pref_code VALUES('11','éÊ§','Saitama');
-INSERT INTO pref_code VALUES('12','çt§','Chiba');
-INSERT INTO pref_code VALUES('13','s','Tokyo');
-INSERT INTO pref_code VALUES('14','_Þì§','Kanagawa');
-INSERT INTO pref_code VALUES('15','V§','Niigata');
-INSERT INTO pref_code VALUES('16','xR§','Toyama');
-INSERT INTO pref_code VALUES('17','Îì§','Ishikawa');
-INSERT INTO pref_code VALUES('18','ä§','Fukui');
-INSERT INTO pref_code VALUES('19','R§','Yamanashi');
-INSERT INTO pref_code VALUES('20','·ì§','Nagano');
-INSERT INTO pref_code VALUES('21','ò§','Gifu');
-INSERT INTO pref_code VALUES('22','Ãª§','Shizuoka');
-INSERT INTO pref_code VALUES('23','¤m§','Aichi');
-INSERT INTO pref_code VALUES('24','Od§','Mie');
-INSERT INTO pref_code VALUES('25',' ê§','Shiga');
-INSERT INTO pref_code VALUES('26','s{','Kyoto');
-INSERT INTO pref_code VALUES('27','åã{','Osaka');
-INSERT INTO pref_code VALUES('28','ºÉ§','Hyogo');
-INSERT INTO pref_code VALUES('29','ÞÇ§','Nara');
-INSERT INTO pref_code VALUES('30','aÌR§','Wakayama');
-INSERT INTO pref_code VALUES('31','¹æ§','Tottori');
-INSERT INTO pref_code VALUES('32','ª§','Shimane');
-INSERT INTO pref_code VALUES('33','ªR§','Okayama');
-INSERT INTO pref_code VALUES('34','L§','Hiroshima');
-INSERT INTO pref_code VALUES('35','Rû§','Yamaguchi');
-INSERT INTO pref_code VALUES('36','¿§','Tokushima');
-INSERT INTO pref_code VALUES('37','ì§','Kagawa');
-INSERT INTO pref_code VALUES('38','¤Q§','Ehime');
-INSERT INTO pref_code VALUES('39','m§','Kochi');
-INSERT INTO pref_code VALUES('40','ª§','Fukuoka');
-INSERT INTO pref_code VALUES('41','²ê§','Saga');
-INSERT INTO pref_code VALUES('42','·è§','Nagasaki');
-INSERT INTO pref_code VALUES('43','F{§','Kumamoto');
-INSERT INTO pref_code VALUES('44','åª§','Oita');
-INSERT INTO pref_code VALUES('45','{è§','Miyazaki');
-INSERT INTO pref_code VALUES('46','­§','Kagoshima');
-INSERT INTO pref_code VALUES('47','«ê§','Okinawa');
-
+--make_covit19_2.sql
 CREATE TABLE covid_19
 AS 
 SELECT c.reporting_date AS reporting_date,
